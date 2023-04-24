cff6ecdccafef6ae72be5603d787acc05476506f
Benedicttirkey <benedicttirkey05@gmail.com>
2023-04-24 23:39:52 +0530

    port modify

--- application.py
+++ application.py
@@ -12,7 +12,7 @@ app=application
 ##route for a home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
@@ -41,4 +41,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8000)        
+    app.run(host="0.0.0.0",port=8080)        
