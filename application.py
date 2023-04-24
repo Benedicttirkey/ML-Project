4749b0ed841cc2e6bb5b4f1d793a2d95451fe309
Benedicttirkey <benedicttirkey05@gmail.com>
2023-04-24 23:02:28 +0530

    port modify

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from Flask import Flask,request,render_template
+from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
@@ -41,4 +41,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    app.run(host="0.0.0.0",port=8000)        
